f94bf436ff13dff9ce34378dac1703f92ff01a87
Quocviet Le <quocvietlet@gmail.com>
2024-07-29 22:00:30 -0500

    new comments

--- msdie.py
+++ msdie.py
@@ -1,34 +1,35 @@
 # msdie.py
-"""Class definition for an n-sided die."""
+# Class definition for an n-sided die.
 
 from random import randrange
 
 
 class MSDie:
-    """A class to represent a multi-sided die."""
+    # A class to represent a multi-sided die.
 
     def __init__(self, n):
-        """Creates a multi-sided die with the given number of sides
-        as a parameter."""
+        # Creates a multi-sided die with the given number of sides as a parameter.
         self.sides = n
         self.value = 1
 
     def roll(self):
-        """Rolls the dice to a random face."""
+        # Rolls the dice to a random face.
         self.value = randrange(1, self.sides + 1)
 
     def getValue(self):
-        """Shows the value of the current face."""
+        # Shows the value of the current face.
         return self.value
 
     def setValue(self, val):
-        """Sets the value of the die's face to the given parameter."""
+        # Sets the value of the die's face to the given parameter.
         self.value = val
 
 
+# The main function is used to test the MSDie class.
 def main():
     die1 = MSDie(6)
-    die1.setValue(4)
+    die1.setValue(1)
+    die1.roll()
     die1.getValue()
     print(die1.getValue())
 
